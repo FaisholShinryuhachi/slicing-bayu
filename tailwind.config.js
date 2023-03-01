24f573695f3c173fa3bfd627e276ee457f26a59d
fahmifaishol <fahmifaishol11@gmail.com>
2023-03-02 02:02:29 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "*.{html,js}"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#F19B9B',
+        secondary: '#88D7F8'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      backgroundImage: {
+        'footer': "url('../dist/img/Vector 3.png')",
+      },
+    },
+  },
+  plugins: [
+  ],
+}
